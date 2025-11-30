0b88023ed90abfa383f65002193229ad76e1973e
scorpious-rex <aadityasingh.tit@gmail.com>
2025-11-30 10:39:53 +0530

    updated

--- src/function.java
+++ src/function.java
@@ -1,14 +1,41 @@
-import java.util.*;
-public class function{
-    public static void printMyName(String name){
-        System.out.println(name);
-        return;}
-        public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-            String name = sc.next();
+import java.util.Scanner;
+class first{
+    public static void name1(String a){
+        System.out.print(a+" ");
 
-            printMyName(name);
+    }
+}
 
-
+    class secoend {
+        public static void name2(String b) {
+            System.out.print(b+" ");
         }
+    }
+        class third {
+            public static void name3(String c) {
+                System.out.print(c+" ");
+            }
+        }
+public class function{
+            public static void main() {
+                System.out.println("Enter your First name");
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+                System.out.println("Enter your middle name");
+                String m = sc.nextLine();
+                System.out.println("enter your last name");
+                String v = sc.nextLine();
+
+                first l= new first();
+                l.name1(s);
+
+                secoend x= new secoend();
+                x.name2(m);
+
+                third j= new third();
+                j.name3(v);
+
+
+
+}
 }
