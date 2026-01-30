e4b9efd1362392ae80884cad33aefc64d00afb38
Aaditya <aadityasingh.tit@gmail.com>
2026-01-30 11:10:56 +0530

    My basic java

---
+++ src/SquareLine.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SquareLine {
+    static void main() {
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int line=1; line<=a; line++){
+            System.out.println("****");
+        }
+    }
+}
