1b0aeb0f2c436e5b6ed075057f0ab63b8d026df9
Aaditya <aadityasingh.tit@gmail.com>
2026-01-30 10:30:07 +0530

    My basic java

---
+++ src/loops.java
@@ -0,0 +1,39 @@
+//public class loops {
+//    static void main() {
+//        int a=0;
+//        while(a!=10){
+//            System.out.println("I am A bad boy");
+//            a++ ;
+//        }
+//    }
+//}
+
+//public class loops{
+//    static void main() {
+//        int a=0;
+//        while(a<10){
+//            System.out.println("BAdal bc");
+//            a++;
+//        }
+//    }
+//}
+
+//public class loops{
+//    static void main() {
+//        for(int a=0; a<=10; a++){
+//            System.out.println(a);
+//        }
+//    }
+//}
+
+
+//public class loops {
+//    static void main() {
+//        int a=0;
+//        do{
+//            System.out.println(a);
+//
+//            a++;
+//        }while(a<=10);
+//    }
+//}
